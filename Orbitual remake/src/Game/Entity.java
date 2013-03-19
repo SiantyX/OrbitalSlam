33fb642678987966fe938bd472f25714ef6f68e7
Felix Sandvik <siantyx@gmail.com>
2013-03-19 11:03:22 +0100

    La till entity och en icke påbörjad player, samt testar general anchor(map) funktion

---
+++ Orbitual remake/src/Game/Entity.java
@@ -0,0 +1,102 @@
+package Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.ShapeFill;
+import org.newdawn.slick.fills.GradientFill;
+import org.newdawn.slick.geom.Shape;
+
+public class Entity {
+	public static ArrayList<Entity> entityList = new ArrayList<Entity>();
+	
+	private Image im;
+	private Shape sh;
+	private ShapeFill sf;
+	private Color c;
+	
+	private int x,y;
+	
+	/**
+	 * Adds entity to entityList.
+	 */
+	public Entity() {
+		entityList.add(this);
+		x = y = 0;
+	}
+	
+	/**
+	 * Creates an entity that renders a sprite.
+	 * @param im Image to render
+	 */
+	public Entity(Image im, int x, int y) {
+		this();
+		this.im = im;
+		this.x = x;
+		this.y = y;
+	}
+	
+	/**
+	 * Creates an entity that renders a shape.
+	 * @param sh Shape to render (only outlines)
+	 */
+	public Entity(Shape sh) {
+		this();
+		this.sh = sh;
+	}
+	
+	/**
+	 * Creates an entity that renders a shape with the specified ShapeFill.
+	 * @param sh Shape to render
+	 * @param sf ShapeFill to fill the Shape with
+	 */
+	public Entity(Shape sh, ShapeFill sf) {
+		this();
+		this.sh = sh;
+		this.sf = sf;
+	}
+	
+	/**
+	 * Creates an entity that render a shape with the specified image as texture.
+	 * @param sh Shape to render
+	 * @param im Image to fill the Shape with
+	 */
+	public Entity(Shape sh, Image im) {
+		this();
+		this.sh = sh;
+		this.im = im;
+	}
+	
+
+	/**
+	 * Renders the entity in different ways depending on the content of the entity.
+	 * @param g Graphics to draw to
+	 */
+	public void render(Graphics g) {
+		// sets color before doing anything
+		if(c != null)
+			g.setColor(c);
+		else
+			g.setColor(Color.orange);
+		// -------------------------------
+		
+		if(sh != null) {
+			if(sf != null)
+				g.fill(sh, sf);
+			else if(im != null)
+				g.texture(sh, im);
+			
+			g.fill(sh);
+		}
+		
+		
+		else if(im != null) {
+			im.draw(x, y);
+		}
+	}
+
+}
--- Orbitual remake/src/Game/Game.java
+++ Orbitual remake/src/Game/Game.java
@@ -5,37 +5,52 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.*;
 
 public class Game extends BasicGame{
 	 
-    public Game()
-    {
-        super("Slick2DPath2Glory - SimpleGame");
+	private float w;
+	private float h;
+	
+    public Game() {
+        super("Orbitual Remake");
     }
  
     @Override
-    public void init(GameContainer gc) 
-   throws SlickException {
-
+    public void init(GameContainer gc) throws SlickException {
+    	w = Math.round(gc.getWidth());
+    	h = Math.round(gc.getHeight());
+    	
+    	int numAnc = 12;
+    	
+    	int startPosX = 120;
+    	int numAncPerRow = 4;
+    	
+    	int startPosY = 150;
+    	int numAncPerColumn = numAnc/numAncPerRow;
+    	
+    	Entity[] anchors = new Entity[numAnc];
+    	for(int i = 0; i < numAnc; i++) {
+    		anchors[i] = new Entity(new Circle(startPosX + (i%numAncPerRow) * (((w-(2*startPosX))/(numAncPerRow-1))),
+    				startPosY + (i%numAncPerColumn) * (((h-(2*startPosY))/(numAncPerColumn-1))), 5));
+    	}
+    	
+    	//new Entity(new Rectangle(400, 300, 100 ,100));
     }
  
     @Override
-    public void update(GameContainer gc, int delta) 
-   throws SlickException     
-    {
- 
-    }
- 
-    public void render(GameContainer gc, Graphics g) throws SlickException 
-    {
+    public void update(GameContainer gc, int delta) throws SlickException {
     	
     }
  
-    public static void main(String[] args) 
-   throws SlickException
-    {
-         AppGameContainer app = 
-   new AppGameContainer(new Game());
+    public void render(GameContainer gc, Graphics g) throws SlickException {
+    	for(Entity e : Entity.entityList) {
+    		e.render(g);
+    	}
+    }
+ 
+    public static void main(String[] args) throws SlickException {
+         AppGameContainer app = new AppGameContainer(new Game());
  
          app.setDisplayMode(800, 600, false);
          app.start();
