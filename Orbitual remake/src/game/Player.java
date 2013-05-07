06be80fbda0a36dcfe8ba89cfcac7773d37c6ed4
Felix Sandvik <siantyx@gmail.com>
2013-05-07 08:03:18 +0200

    nu blir man stunnad. behöver bild för att visa det

--- Orbitual remake/src/game/Player.java
+++ Orbitual remake/src/game/Player.java
@@ -24,35 +24,38 @@ public class Player {
 	private final String[] playerImg = new String[]{"res/sprites/smiley1.png", "res/sprites/smiley2.png", "res/sprites/smiley3.png", "res/sprites/smiley4.png", "res/sprites/smiley5.png", "res/sprites/smiley6.png", "res/sprites/smiley7.png", "res/sprites/smiley8.png"};
 	public static final float stdScale = 0.0005208f;
 	private int num;
-	
+
 	private double dx;
 	private double dy;
-	
+
 	private double speed;
 	private double mass;
-	
+
+	private double stunTime;
+
 	// hook variables
 	private boolean hooked;
 	private Entity hookedTo;
-	
+
 	private double wSpeed;
 	private double degrees;
 	private double hookLength;
-	
+
 	private boolean clockWise;
-	
+
 	private double centriAcc;
 	private final double MAXSPINSPEED = 16;
 	private final double ACC_CONST = 300;
-	private final float SPEED_LOST = 0.6f;
 	// -----------------------
-	
+
 	public static ArrayList<Entity> anchorList;
-	
+
 	public int KEYBIND;
-	
+
 	private final float gravity = 0.015f;
-	
+	private final float SPEED_LOST = 0.6f;
+	private final double STUN_LENGTH = 50;
+
 	public Player(Player player) throws SlickException {
 		entity = new Entity(player.getEntity().getId());
 		entity.AddComponent(new ImageRenderComponent("Player " + num, new Image(playerImg[num])));
@@ -63,37 +66,38 @@ public class Player {
 		mass = 1;
 		speed = player.getSpeed();
 	}
-	
+
 	public Player(int num, AnchorMap map) throws SlickException {
 		Vector2f startPos = new Vector2f(map.getStartPosX() + (num) * (((Game.WIDTH-(2*map.getStartPosX()))/(map.getNumAncPerRow()-1))) - Game.WIDTH/14, map.getStartPosY() - Game.HEIGHT/10);
 		entity = new Entity("Player " + num);
 		entity.AddComponent(new ImageRenderComponent("Player " + num, new Image(playerImg[num])));
 		entity.setCenterPosition(startPos);
 		entity.setScale(stdScale*Game.WIDTH);
-		
+
 		dead = false;
-		
-		dx = 10 + num * -15;
+
+		dx = 20 + num * -15;
 		dy = num * -1;
 		speed = 0;
 		mass = 1;
-		
+		stunTime = 0;
+
 		// hook variables
 		hookedTo = null;
 		hooked = false;
-		
+
 		wSpeed = 0;
 		degrees = 0;
 		hookLength = 0;
-		
+
 		clockWise = false;
-		
+
 		centriAcc = 0;
 		// -----------------
-		
+
 		this.num = num;
 	}
-	
+
 	public void update(GameContainer gc, StateBasedGame sb, int delta) {		
 		// check if dead
 		if(entity.getCenterPosition().x < 0 || entity.getCenterPosition().x > Game.WIDTH
@@ -101,16 +105,24 @@ public class Player {
 			dead = true;
 		}
 		if(dead) return;
-		
-		// hook button
-		Input input = gc.getInput();
-		if (input.isKeyPressed(KEYBIND)) {
-			hooked = !hooked;
-			if(hooked) {
-				hook();
+
+		if(stunTime != 0) {
+			stunTime -= delta;
+			if(stunTime < 0) {
+				stunTime = 0;
 			}
 		}
-		
+		else {
+			// hook button
+			Input input = gc.getInput();
+			if (input.isKeyPressed(KEYBIND)) {
+				hooked = !hooked;
+				if(hooked) {
+					hook();
+				}
+			}
+		}
+
 		// fall
 		if(!hooked) {
 			dy += gravity*delta;
@@ -125,7 +137,7 @@ public class Player {
 			}
 			dx = hookedTo.getCenterPosition().x + Math.cos(degrees*Math.PI/180) * hookLength - entity.getCenterPosition().x;
 			dy = hookedTo.getCenterPosition().y - Math.sin(degrees*Math.PI/180) * hookLength - entity.getCenterPosition().y;
-			
+
 			if(wSpeed >= MAXSPINSPEED) {
 				wSpeed = MAXSPINSPEED;
 			}
@@ -133,13 +145,12 @@ public class Player {
 				wSpeed += centriAcc;
 			}
 		}
-		
+
 		// move
 		speed = Math.hypot(dx, dy);
 		entity.translate((float)dx, (float)dy);
-		//entity.setCenterPosition(new Vector2f( entity.getCenterPosition().x + (float)dx, entity.getCenterPosition().y + (float)dy));
 	}
-	
+
 	private void hook() {
 		// get closest anchor
 		hookLength = 10000;
@@ -150,10 +161,10 @@ public class Player {
 				hookedTo = e;
 			}
 		}
-		
+
 		degrees = Math.atan2(hookedTo.getCenterPosition().y - entity.getCenterPosition().y, 
 				-(hookedTo.getCenterPosition().x - entity.getCenterPosition().x)) * 180/Math.PI;
-		
+
 		// --------------
 		// clockwise
 		double e1x, e1y, e2x, e2y;
@@ -164,12 +175,12 @@ public class Player {
 		p2y = entity.getCenterPosition().y + 99999*dy;
 		p3x = hookedTo.getCenterPosition().x;
 		p3y = hookedTo.getCenterPosition().y;
-		
+
 		e1x = p1x-p2x;
 		e1y = p1y-p2y;
 		e2x = p3x-p2x;
 		e2y = p3y-p2y;
-		
+
 		if((e1x * e2y - e1y * e2x) >= 0) {
 			clockWise = false;
 		}
@@ -180,22 +191,22 @@ public class Player {
 		// speed reduction at weird angles
 		double tmpSpeed = Math.hypot(Math.cos(degrees*Math.PI/180) * dy, Math.sin(degrees*Math.PI/180) * dx);
 		wSpeed = tmpSpeed / hookLength * 180/Math.PI;
-		
+
 		p1x = hookedTo.getCenterPosition().x;
 		p1y = hookedTo.getCenterPosition().y;
-		
+
 		p2x = entity.getCenterPosition().x;
 		p2y = entity.getCenterPosition().y;
-		
+
 		p3x = entity.getCenterPosition().x + 100*dx;
 		p3y = entity.getCenterPosition().y + 100*dy;
-		
+
 		e1x = p2x - p1x;
 		e1y = p2y - p1y;
-		
+
 		e2x = p2x - p3x;
 		e2y = p2y - p3y;
-		
+
 		double test = ((e1x*e2x) + (e1y*e2y)) / (Math.hypot(e1x, e1y) * Math.hypot(e2x, e2y));
 		test = Math.acos(test) * 180/Math.PI;
 		if(test > 90) {
@@ -203,58 +214,65 @@ public class Player {
 		}
 		wSpeed = test/90 * wSpeed;
 		// ----------------------------------
-		
+
 		centriAcc = ACC_CONST/(hookLength*hookLength);
 	}
-	
+
 	public void render(GameContainer gc, StateBasedGame sb, Graphics g) {
 		if(dead) return;
-		
+
 		if(hooked) {
 			g.setColor(Color.white);
 			g.drawLine(entity.getCenterPosition().x, entity.getCenterPosition().y, hookedTo.getCenterPosition().x, hookedTo.getCenterPosition().y);
 		}
 		entity.render(gc, sb, g);
+		
+		// debug stun time
+		//g.drawString(new Double(stunTime).toString(), entity.getPosition().x, entity.getPosition().y);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Player " + new Integer(num + 1).toString();
 	}
-	
+
 	public boolean isDead() {
 		return dead;
 	}
-	
+
 	public Entity getEntity() {
 		return entity;
 	}
-	
+
 	public double getDx() {
 		return dx;
 	}
-	
+
 	public double getDy() {
 		return dy;
 	}
-	
+
 	public double getSpeed() {
 		return speed;
 	}
-	
+
 	public double getMass() {
 		return mass;
 	}
-	
+
 	public Vector2f getVelocity() {
 		return new Vector2f((float)dx, (float)dy);
 	}
-	
+
 	public void setVelocity(Vector2f v) {
 		dx = v.x;
 		dy = v.y;
 	}
-	
+
+	public void setStunTime(double time) {
+		stunTime = time;
+	}
+
 	// COLLISION PHYSICS
 	public void collision(Player player) {
 		// ------------------------------------------------------------------------------------------------------
@@ -263,43 +281,62 @@ public class Player {
 		Vector2f delta = new Vector2f(entity.getCenterPosition().x - player.getEntity().getCenterPosition().x, entity.getCenterPosition().y - player.getEntity().getCenterPosition().y);
 		float r = entity.getRadius() + player.entity.getRadius();
 		float dist2 = delta.dot(delta);
-		
+
 		if(dist2 > r*r) return;
-		
+
 		float d = delta.length();
-		
+
 		Vector2f mtd;
 		if(d == 0.0f) {
 			d = player.entity.getRadius() + entity.getRadius() - 1;
 			delta = new Vector2f(player.entity.getRadius() + entity.getRadius(), 0);
 		}
-		
+
 		mtd = new Vector2f(delta.x * (((entity.getRadius() + player.entity.getRadius()) - d)/d), delta.y * (((entity.getRadius() + player.entity.getRadius()) - d)/d));
-		
+
 		float im1 = (float) (1 / getMass());
 		float im2 = (float) (1 / player.getMass());
-		
+
 		Vector2f mtdScaled1 = new Vector2f(mtd.x * (im1 / (im1 + im2)), mtd.y * (im1 / (im1 + im2)));
 		Vector2f mtdScaled2 = new Vector2f(mtd.x * (im2 / (im1 + im2)), mtd.y * (im2 / (im1 + im2)));
 		entity.setCenterPosition(new Vector2f(entity.getCenterPosition().x + mtdScaled1.x, entity.getCenterPosition().y + mtdScaled1.y));
 		player.getEntity().setCenterPosition(new Vector2f(player.getEntity().getCenterPosition().x + mtdScaled2.x, player.getEntity().getCenterPosition().y + mtdScaled2.y));
-		
+
 		Vector2f v = new Vector2f((float)dx - player.getVelocity().x, (float)dy - player.getVelocity().y);
-		
+
 		float vn = v.dot(mtd.normalise());
-		
+
 		if(vn > 0.0f) return;
-		
+
 		// impulse
 		float i = (-(1.0f + SPEED_LOST) * vn) / (im1 + im2);
 		Vector2f impulse = new Vector2f(mtd.x * i, mtd.y * i);
-				
+
 		// momentum
 		Vector2f dim1 = new Vector2f(impulse.x * im1, impulse.y * im1);
 		Vector2f dim2 = new Vector2f(impulse.x * im2, impulse.y * im2);
-		setVelocity(new Vector2f((float)dx + dim1.x, (float)dy + dim1.y));
-		player.setVelocity(new Vector2f(player.getVelocity().x - dim2.x, player.getVelocity().y - dim2.y));
+		Vector2f newV = new Vector2f((float)dx + dim1.x, (float)dy + dim1.y);
+		Vector2f otherNewV = new Vector2f(player.getVelocity().x - dim2.x, player.getVelocity().y - dim2.y);
 		// ------------------------------------------------------------------------------------------------------
 		// ------------------------------------------------------------------------------------------------------
+
+		hooked = false;
+
+		double dSpeed = 0;
+		if(getSpeed() < player.getSpeed()) {
+			// this stunnad
+			// stunduration i skildnad
+			dSpeed = Math.abs(getSpeed() - player.getSpeed());
+			stunTime = dSpeed*STUN_LENGTH;
+		}
+		else if(getSpeed() > player.getSpeed()){
+			// player stunnad
+			// stunduration i skildnad
+			dSpeed = Math.abs(getSpeed() - player.getSpeed());
+			player.setStunTime(dSpeed*STUN_LENGTH);
+		}
+
+		setVelocity(newV);
+		player.setVelocity(otherNewV);
 	}
 }
