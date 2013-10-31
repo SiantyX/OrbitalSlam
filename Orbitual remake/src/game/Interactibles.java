2320cb4ce2666208b6462e68295507d06b55bcf4
magnificus <tobiaselinder@gmail.com>
2013-10-31 12:59:15 +0100

    minbusiness yo

---
+++ Orbitual remake/src/game/Interactibles.java
@@ -0,0 +1,14 @@
+package game;
+
+public abstract class Interactibles extends Entity {
+
+	public Interactibles(String id) {
+		super(id);
+		
+	}
+	
+	public abstract void collisioncheck(){
+		
+	}
+
+}
